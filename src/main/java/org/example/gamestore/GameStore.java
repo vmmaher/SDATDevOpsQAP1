4ee37bc09a84a59730d7a2621dbe611fad478bcb
Vanessa Maher <vanessa.maher@keyin.com>
2025-05-23 15:31:34 -0230

    Added addToCart method as well as placeholder methods.

--- src/main/java/org/example/gamestore/GameStore.java
+++ src/main/java/org/example/gamestore/GameStore.java
@@ -2,9 +2,11 @@ package org.example.gamestore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class GameStore {
-    private List<Game> games;
+    private final List<Game> games;
+    private final List<Game> cart = new ArrayList<>();
 
     public GameStore() {
         games = new ArrayList<>();
@@ -22,7 +24,46 @@ public class GameStore {
         for (Game game : games) {
             System.out.printf("%s - $%.2f (%d in stock) %n",
                     game.getName(), game.getPrice(), game.getQuantity());
-      ;  }
+        }
+    }
+
+    public boolean addToCart(String gameTitle) {
+        // using optional for easier search
+        Optional<Game> foundGameOptional = games.stream()
+                .filter(game -> game.getName().equalsIgnoreCase(gameTitle))
+                .findFirst();
+
+        if (foundGameOptional.isPresent()) {
+            Game game = foundGameOptional.get();
+
+            if (game.getQuantity() <= 0) {
+                System.out.println("This game is currently out of stock. Sorry!");
+                return false;
+            }
+
+            game.setQuantity(game.getQuantity() - 1);
+            cart.add(game);
+            System.out.println(game.getName() + " added to cart.");
+            return true;
+        } else {
+                System.out.println(gameTitle + " wasn't found!");
+                return false;
+        }
+    }
+
+    public boolean removeFromCart(String gameTitle) {
+        // placeholder
+        return false;
+    }
+
+    public double calculateCartTotal() {
+        // placeholder
+        return 0.0;
+    }
+
+    public boolean purchaseGames() {
+        // placeholder
+        return false;
     }
 }
 
